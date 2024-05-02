8da6115f972a1a69fe72c76e5ea86d20bef07fc8
Abhishek Karimpuzha Devarajan <66976767+ABeeShake@users.noreply.github.com>
2024-05-02 09:09:23 -0700

    Add files via upload

--- error_distribution.py
+++ error_distribution.py
@@ -13,33 +13,35 @@ if __name__ == "__main__":
 
     path, num_samples = sys.argv[1:]
 
+    data = pd.read_csv(path)
+
+    pids = data["patientid"].unique()
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    X,_ = d.get_cgm_vars(path, device)
-
-    dim = X.shape[-1]
 
     start = 20
 
     for i in range(int(num_samples)):
 
+        X,y = d.get_cgm_vars(path, device, pid = pids[i])
+
         end = len(X)
+
+        eta = np.sqrt(np.log(3)/(end - start))
 
         forecastsi = sim.run_sim(start, end, device, X, y = None, save = False)
 
-        ftli, lossesi, ftl_lossesi, leaderi = sim.full_loss_forecasts(forecastsi, y = None, start = start, end = end)
-        ftl2i, losses2i, leader2i = sim.leader_loss_forecasts(forecastsi, y = None, start = start, end = end)
+        ftli, lossesi, ftl_lossesi, leaderi = sim.full_loss_forecasts(forecastsi, y, start = start, end = end, device = device, eta = eta)
+        ftl2i, losses2i, leader2i = sim.leader_loss_forecasts(forecastsi, y, start = start, end = end, device = device, eta = eta)
 
         ftl_losses2i = losses2i.flatten()[losses2i.flatten().nonzero()]
 
         with open("./losses/full_losses.csv", "a") as f:
     
             w = csv.writer(f)
-            w.writerows(ftl_lossesi.tolist())
+            w.writerows(ftl_lossesi.T.tolist())
 
         with open("./losses/leader_losses.csv", "a") as f:
     
             w = csv.writer(f)
-            w.writerows(ftl_losses2i.tolist())
-
-        X,_ = d.get_cgm_vars(path, device)
+            w.writerows(ftl_losses2i.T.tolist())
